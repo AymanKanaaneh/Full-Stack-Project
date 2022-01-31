abc36e0d71eb4af3570e26ff6c8a35c16a9f7cd0
Ayman <57593703+AymanKanaaneh@users.noreply.github.com>
2022-01-31 23:28:12 +0200

    Delete SqlCode2 directory

--- SqlCode2/dbo.Exams.data.sql
+++
@@ -1,9 +0,0 @@
-SET IDENTITY_INSERT [dbo].[Exams] ON
-INSERT INTO [dbo].[Exams] ([Id], [Title], [DateStarted], [DurationMinutes], [TeacherId]) VALUES (7, N'c#', N'2015-05-16 05:50:06', 1, 1)
-INSERT INTO [dbo].[Exams] ([Id], [Title], [DateStarted], [DurationMinutes], [TeacherId]) VALUES (1004, N'c++', N'2015-05-16 05:50:06', 55, 1)
-INSERT INTO [dbo].[Exams] ([Id], [Title], [DateStarted], [DurationMinutes], [TeacherId]) VALUES (1005, N'fullstack', N'2015-05-16 05:50:06', 60, 1)
-INSERT INTO [dbo].[Exams] ([Id], [Title], [DateStarted], [DurationMinutes], [TeacherId]) VALUES (1008, N'fullstack', N'2015-05-16 05:50:06', 45, 3)
-INSERT INTO [dbo].[Exams] ([Id], [Title], [DateStarted], [DurationMinutes], [TeacherId]) VALUES (1009, N'Python', N'2021-03-22 16:05:09', 45, 3)
-INSERT INTO [dbo].[Exams] ([Id], [Title], [DateStarted], [DurationMinutes], [TeacherId]) VALUES (2009, N'C#', N'2021-03-29 00:00:00', 60, 3)
-INSERT INTO [dbo].[Exams] ([Id], [Title], [DateStarted], [DurationMinutes], [TeacherId]) VALUES (2011, N'Mort', N'2021-03-29 16:20:00', 60, 3)
-SET IDENTITY_INSERT [dbo].[Exams] OFF
